64f12ce08384fb669bae358d6caae1010b717588
Nicolas Gaillard <nicolas.gaillard@etu.univ-nantes.fr>
2017-01-13 14:59:26 +0100

    add liste cheum

---
+++ dataset_seances.pl
@@ -0,0 +1,99 @@
+seances(multimedia, cm, [id4], [jpguedon],
+	[s1,s6,s29,s63,s64]).
+seances(multimedia, tp, [id4], [mgelgon],
+	[s43]).
+
+
+seances(cpp, cm, [id4,silr1,silr2], [fpicarougne],
+	[s2,s17,s27,s36,s54,s58]).
+seances(cpp, tp, [id4], [srouibia],
+	[s57]).
+seances(cpp, tp, [silr1], [amilliat],
+	[s61]).
+seances(cpp, tp, [silr2], [amilliat],
+	[s65]).
+
+
+seances(genieLogiciel, cm, [id4,silr1,silr2], [mbarkowsky],
+	[s3,s4,s25,s37,s45,s52]).
+seances(genieLogiciel, cm, [id4,silr1,silr2], [yprie],
+	[s5,s7]).
+seances(genieLogiciel, td, [silr2], [yprie], 
+	[s8]).
+seances(genieLogiciel, td, [silr1], [yprie], 
+	[s12]).
+seances(genieLogiciel, td, [id4], [yprie], 
+	[s18]).
+
+
+seances(bdd, cm, [id4,silr1,silr2], [graschia],
+	[s9,s21,s30,s35,s62]).
+seances(bdd,td,[silr1], [mpnachouki],
+	[s34,s76]).
+seances(bdd,td,[silr2], [mpnachouki],
+	[s39,s70]).
+seances(bdd,td,[id4], [graschia],
+	[s41,s72]).
+seances(bdd,tp,[id4], [graschia],
+	[s46,s47]).
+
+
+seances(modeleAleatoire, cm, [silr1,silr2], [vricordel],
+	[s10,s13,s19,s28]).
+seances(modeleAleatoire, td, [silr1], [vricordel],
+	[s15, s31]).
+seances(modeleAleatoire, td, [silr2], [bparrein],
+	[s16,s32]).
+
+
+seances(progPara, cm, [id4], [jmartinez], 
+	[s11,s14]).
+seances(progPara, td, [id4], [jmartinez], 
+	[s26,s33]).
+seances(progPara, td, [id4], [jmartinez], 
+	[s26,s33]).
+seances(progPara, ds, [id4], [jmartinez], 
+	[s69]).
+
+
+seances(analDonnees, cm, [id4], [pkuntz],
+	[s20,s22,s38,s53,s60,s73]).
+
+
+seances(ptrans, cm, [id4,silr1,silr2], [jpguedon],
+	[s23]).
+seances(ptrans, cm, [id4,silr1,silr2], [mbarkowsky],
+	[s24]).
+
+
+seances(gestionProjet, td, [id4], [sporcheron],
+	[s40]).
+seances(gestionProjet, td, [silr1], [sporcheron],
+	[s42]).
+seances(gestionProjet, td, [silr1], [sporcheron],
+	[s44]).
+
+
+seances(marketing, cm, [silr1,silr2], [loili],
+	[s48,s49,s74,s75]).
+seances(marketing, td, [id4], [loili],
+	[s50,s51,s77,s78]).
+
+
+seances(patrons, [id4,silr1,silr2], [apigeau],
+	[s55,s66,s71]).
+
+
+seances(anglais, [id4,silr1,silr2], [profAnglais],
+	[s56]).
+
+
+seances(sport, [id4,silr1,silr2], [profSport],
+	[s59]).
+
+
+seances(ptrans, [id4,silr1,silr2], [profPtrans],
+	[s67,s68]).
+
+% Les options (Espagnol, Anglais Renforcé) ne sont pas présentes ainsi que les cours de Français
+% Les tiers temps ne sont pas comptabilisés
